68ee9fcb3eaed266afcd50d03fdd806e158e744b
njoize <njoize@gmail.com>
2018-11-24 15:49:20 +0700

    Learning Connected Printer

--- app/src/main/java/njoize/dai_ka/com/demotestprint/MainFragment.java
+++ app/src/main/java/njoize/dai_ka/com/demotestprint/MainFragment.java
@@ -1,11 +1,18 @@
 package njoize.dai_ka.com.demotestprint;
 
 
+import android.app.ExpandableListActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.zj.wfsdk.WifiCommunication;
 
 
 /**
@@ -13,10 +20,47 @@ import android.view.ViewGroup;
  */
 public class MainFragment extends Fragment {
 
+//    Explicit
+    private WifiCommunication wifiCommunication;
+
 
     public MainFragment() {
         // Required empty public constructor
     }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Check Connected Printer
+        wifiCommunication = new WifiCommunication(handler);
+        wifiCommunication.initSocket( "192.168.1.87", 9100);
+
+    } // Main Method
+
+    private Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            String tag = "24novV3";
+            switch (msg.what) {
+
+                case WifiCommunication.WFPRINTER_CONNECTED:
+                    Log.d(tag, "Success Connected Printer");
+                    break;
+                case WifiCommunication.WFPRINTER_DISCONNECTED:
+                    Log.d(tag, "Disconnected Printer");
+                    break;
+                default:
+                    break;
+
+            } // switch
+
+        } // handleMessage
+    };
 
 
     @Override
