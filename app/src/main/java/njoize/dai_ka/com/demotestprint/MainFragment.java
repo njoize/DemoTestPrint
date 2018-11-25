69c99375ae2ea6265e7463f0afd680cd21a29f77
njoize <njoize@gmail.com>
2018-11-25 10:28:46 +0700

    Can Print Success

--- app/src/main/java/njoize/dai_ka/com/demotestprint/MainFragment.java
+++ app/src/main/java/njoize/dai_ka/com/demotestprint/MainFragment.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.zj.wfsdk.WifiCommunication;
 
@@ -22,11 +24,12 @@ public class MainFragment extends Fragment {
 
 //    Explicit
     private WifiCommunication wifiCommunication;
+    private boolean aBoolean = false;
+    private boolean communicationABoolean = true; // true ==> Can Print, false ==> Disable Print
+    private Button button, printAgainButton;
 
 
-    public MainFragment() {
-        // Required empty public constructor
-    }
+    public MainFragment() {}
 
 
     @Override
@@ -34,10 +37,26 @@ public class MainFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
 //        Check Connected Printer
-        wifiCommunication = new WifiCommunication(handler);
-        wifiCommunication.initSocket( "192.168.1.87", 9100);
+        createCommunicationPrinter();
+
+//        Print Controller
+        button = getView().findViewById(R.id.btnPrint);
+        printAgainButton = getView().findViewById(R.id.btnPrintAgain);
+        printAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createCommunicationPrinter();
+                communicationABoolean = true;
+            }
+        });
+
 
     } // Main Method
+
+    private void createCommunicationPrinter() {
+        wifiCommunication = new WifiCommunication(handler);
+        wifiCommunication.initSocket( "192.168.1.87", 9100);
+    }
 
     private Handler handler = new Handler() {
 
@@ -50,6 +69,37 @@ public class MainFragment extends Fragment {
 
                 case WifiCommunication.WFPRINTER_CONNECTED:
                     Log.d(tag, "Success Connected Printer");
+                    button.setText("Test Print");
+
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            if (communicationABoolean) {
+
+                                String printSring = "Doramon";
+                                Log.d("24novV3", "You Click Test Print" + printSring);
+
+                                byte[] bytes = new byte[]{0x10, 0x04, 0x04};
+                                wifiCommunication.sndByte(bytes);
+                                wifiCommunication.sendMsg(printSring,"gbk");
+
+                                byte[] bytes1 = new byte[]{0x0A, 0x0D};
+                                wifiCommunication.sndByte(bytes1);
+
+                                wifiCommunication.close();
+
+                                communicationABoolean = false;
+
+                            } else {
+
+                                //Log.d("24novV3", "Communication Disible");
+                                Toast.makeText(getActivity(), "Disable Printer Please Press Click Again", Toast.LENGTH_SHORT).show();
+                            }
+
+                        } // onClick
+                    });
+
                     break;
                 case WifiCommunication.WFPRINTER_DISCONNECTED:
                     Log.d(tag, "Disconnected Printer");
