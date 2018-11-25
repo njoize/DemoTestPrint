dfeac9df999616a4ae0268ebd65018c5462d8fd5
njoize <njoize@gmail.com>
2018-11-25 10:54:30 +0700

    Update Line Success

--- app/src/main/java/njoize/dai_ka/com/demotestprint/MainFragment.java
+++ app/src/main/java/njoize/dai_ka/com/demotestprint/MainFragment.java
@@ -27,6 +27,7 @@ public class MainFragment extends Fragment {
     private boolean aBoolean = false;
     private boolean communicationABoolean = true; // true ==> Can Print, false ==> Disable Print
     private Button button, printAgainButton;
+    private int anInt = 0;
 
 
     public MainFragment() {}
@@ -77,15 +78,29 @@ public class MainFragment extends Fragment {
 
                             if (communicationABoolean) {
 
+                                anInt += 1;
+
                                 String printSring = "Doramon";
+                                String line2String = "Thailand";
+                                printSring = printSring + " " + Integer.toString(anInt);
                                 Log.d("24novV3", "You Click Test Print" + printSring);
 
-                                byte[] bytes = new byte[]{0x10, 0x04, 0x04};
+
+
+                                byte[] bytes = new byte[]{0x10, 0x04, 0x04}; // Space Front Bill
                                 wifiCommunication.sndByte(bytes);
                                 wifiCommunication.sendMsg(printSring,"gbk");
 
-                                byte[] bytes1 = new byte[]{0x0A, 0x0D};
+
+
+                                byte[] bytes1 = new byte[]{0x0A, 0x0D}; // Update Line
                                 wifiCommunication.sndByte(bytes1);
+
+//                                    Line2
+                                wifiCommunication.sndByte(bytes);
+                                wifiCommunication.sendMsg(line2String, "gbk");
+                                wifiCommunication.sndByte(bytes1);
+
 
                                 wifiCommunication.close();
 
